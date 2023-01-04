764a44f9ff0ebb094fe3f3a54e72eb4432816d21
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-01-04 12:12:27 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  // let reverseString = n.toString();
+
+  let absNumber = Math.abs(n);
+
+  let reverseNumber = absNumber.toString().split("").reverse().join("");
+
+  return Number(reverseNumber);
+};
